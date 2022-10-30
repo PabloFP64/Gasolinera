840a741c778bcd6b193d84799557550560aaeb55
PabloFP64 <PabloFP64@gmail.com>
2022-10-30 20:30:41 +0100

    LinkedList

--- src/main/java/com/example/gasolinera/Gasolinera/Caja.java
+++ src/main/java/com/example/gasolinera/Gasolinera/Caja.java
@@ -1,10 +1,11 @@
 package com.example.gasolinera.Gasolinera;
 
+import java.util.LinkedList;
 import java.util.Random;
 
 
 public class Caja {
-
+    LinkedList<Integer> coches = new LinkedList<Integer>();
     private Random random = new Random();
 
     private int id;
@@ -18,6 +19,7 @@ public class Caja {
     public synchronized void entraCaja(int id_f) throws InterruptedException{
         while(!libre)
             this.wait();
+        coches.add(id_f);
         System.out.println("El coche " + (id_f+1) + " entra en la caja");
         //if (Clase10Control.getjTextArea_Log()!=null)System.out.println("El coche " + (id_f+1) + " entra en la caja");
         libre = false;
@@ -26,6 +28,7 @@ public class Caja {
 
     public synchronized void saleCaja(int id_f) throws InterruptedException {
         libre = true;
+        coches.remove(id_f);
         System.out.println("El coche " + (id_f+1) + " sale de la caja");
         //if (Clase10Control.getjTextArea_Log()!=null) System.out.println("El coche " + (id_f+1) + " sale de la caja");
         this.notify();
